6cec1dd203355cedbf433268dec2e4f3f147aa06
NguyenHuuPhong <phong112068@gmail.com>
2025-08-01 18:13:07 +0700

    01/08/2025

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -4,13 +4,19 @@ import LeftNav from '../Components/leftNAV';
 export default function AppLayout({ children, user, title }) {
   return (
     <div className="min-h-screen flex flex-col">
+      {/* TOP NAV */}
+      <div className="flex flex-1 flex-col">
+          <main className="pt-[80px] bg-red-100 min-h-screen">
+            {children}
+           
+          </main>
+      </div> 
 
-
-      <header className="bg-white fixed top-0 z-10 w-[100%]">
+      <header className="bg-white fixed  top-0 z-10 w-[195px] ">
         <div className="flex mx-auto items-center justify-between px-4 py-2">
           
           {/* Cột 1: Logo */}
-          <div className="w-1/3 ml-[50px] flex justify-start items-center">
+          <div className=" ml-[50px] flex justify-start items-center">
             <a href="/Schedual" className="flex items-center">
               <img
                 src="/img/iconstella.svg"
@@ -20,34 +26,25 @@ export default function AppLayout({ children, user, title }) {
             </a>
           </div>
 
-        
+         
 
-          {/* Cột 2: Tiêu đề */}
-          <div className="w-1/3 text-center">
+        </div>
+      </header>
+
+      {/* Cột 2: Tiêu đề */}
+          {/* <div className="w-1/3 text-center">
             <h4 className="text-lg font-semibold text-yellow-600">{title}</h4>
-          </div>
+          </div> */}
 
           {/* Cột 3: User Info */}
-          <div className="w-1/3 mr-[100px] flex justify-end text-right">
+          {/* <div className="w-1/3 mr-[50px] flex justify-end text-right">
             {user && (
               <div>
                 <div>🧑‍💼 {user.fullName}</div>
                 <div>🛡️ {user.userGroup}</div>
               </div>
             )}
-          </div>
-        </div>
-
-
-      </header>
-
-      {/* TOP NAV */}
-      <div className="flex flex-1 flex-col">
-          <main className="pt-[2%] bg-white-100 min-h-screen">
-            {children}
-          </main>
-      </div> 
-
+          </div> */}
 
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-        input: ['resources/js/app.jsx'], 
+        input: [
+            'resources/css/calendar.css',
+            'resources/js/app.jsx'], 
         refresh: true,
         }),
         react(),
