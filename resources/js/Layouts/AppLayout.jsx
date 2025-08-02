33a269743cb53fac61290eddd6c20ae4c9da2ae6
NguyenHuuPhong <phong112068@gmail.com>
2025-08-02 10:11:44 +0700

    test 020825

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -4,19 +4,13 @@ import LeftNav from '../Components/leftNAV';
 export default function AppLayout({ children, user, title }) {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* TOP NAV */}
-      <div className="flex flex-1 flex-col">
-          <main className="pt-[80px] bg-red-100 min-h-screen">
-            {children}
-           
-          </main>
-      </div> 
 
-      <header className="bg-white fixed  top-0 z-10 w-[195px] ">
+
+      <header className="bg-white fixed  top-0 z-10 w-[100%] ">
         <div className="flex mx-auto items-center justify-between px-4 py-2">
           
           {/* Cột 1: Logo */}
-          <div className=" ml-[50px] flex justify-start items-center">
+          <div className="w-1/3 ml-[50px] flex justify-start items-center">
             <a href="/Schedual" className="flex items-center">
               <img
                 src="/img/iconstella.svg"
@@ -26,25 +20,32 @@ export default function AppLayout({ children, user, title }) {
             </a>
           </div>
 
-         
-
-        </div>
-      </header>
-
       {/* Cột 2: Tiêu đề */}
-          {/* <div className="w-1/3 text-center">
+          <div className="w-1/3 text-center">
             <h4 className="text-lg font-semibold text-yellow-600">{title}</h4>
-          </div> */}
+          </div>
 
           {/* Cột 3: User Info */}
-          {/* <div className="w-1/3 mr-[50px] flex justify-end text-right">
+          <div className="w-1/3 mr-[50px] flex justify-end text-right">
             {user && (
               <div>
                 <div>🧑‍💼 {user.fullName}</div>
                 <div>🛡️ {user.userGroup}</div>
               </div>
             )}
-          </div> */}
+          </div>
+
+        </div>
+      </header>
+
+
+            {/* TOP NAV */}
+      <div className="flex flex-1 flex-col">
+          <main className="pt-[3%] bg-white-100 min-h-screen">
+            {children}
+           
+          </main>
+      </div> 
 
     </div>
   );
