18a9433c7cd38f5b791f9c32d629c63ab8901714
NguyenHuuPhong <phong112068@gmail.com>
2025-09-17 17:04:04 +0700

    17092025

--- resources/js/Pages/FullCalender.jsx
+++ resources/js/Pages/FullCalender.jsx
@@ -933,59 +933,32 @@ import Selecto from "react-selecto";
         dateClick ={ handleEventUnHightLine}
         eventAllow = {finisedEvent}
 
+ 
         // datesSet={(info) => {
-        //     const { start, end } = info; 
-        //     Swal.fire({
-        //       title: "Đang tải...",
-        //       allowOutsideClick: false,
-        //       didOpen: () => {
-        //         Swal.showLoading();
-        //       },
-        //     });
-           
-        //     router.put(`/Schedual/view`,
-        //       { start: start.toISOString(), end: end.toISOString() }, // ✅ lấy từ info
-        //       {
-        //         preserveState: true,
-        //         preserveScroll: true,
-        //         replace: false,
-        //         only: ['resources', 'sumBatchByStage'],
-        //         onSuccess: () => {
-        //           setTimeout(() => {
-        //             Swal.close();
-        //           }, 500);
-        //         }
+        //   const { start, end } = info; 
+        //   Swal.fire({
+        //     title: "Đang tải...",
+        //     allowOutsideClick: false,
+        //     didOpen: () => {
+        //       Swal.showLoading();
+        //     },
+        //   });
+
+        //   router.put(`/Schedual/view`,
+        //     { start: start.toISOString(), end: end.toISOString() },
+        //     {
+        //       preserveState: true,
+        //       preserveScroll: true,
+        //       replace: false,
+        //       only: ['resources', 'sumBatchByStage'],
+        //       onSuccess: () => {
+        //         setTimeout(() => Swal.close(), 500);
         //       }
-        //     );
+        //     }
+        //   );
 
-  
-          
+        //   // ✅ Bỏ luôn phần Inertia.visit vì không cần thiết
         // }}
-        datesSet={(info) => {
-          const { start, end } = info; 
-          Swal.fire({
-            title: "Đang tải...",
-            allowOutsideClick: false,
-            didOpen: () => {
-              Swal.showLoading();
-            },
-          });
-
-          router.put(`/Schedual/view`,
-            { start: start.toISOString(), end: end.toISOString() },
-            {
-              preserveState: true,
-              preserveScroll: true,
-              replace: false,
-              only: ['resources', 'sumBatchByStage'],
-              onSuccess: () => {
-                setTimeout(() => Swal.close(), 500);
-              }
-            }
-          );
-
-          // ✅ Bỏ luôn phần Inertia.visit vì không cần thiết
-        }}
 
 
         resourceGroupField="stage"
