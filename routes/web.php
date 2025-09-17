cf59b2f4184f7558ba774869b526f3b70466b50f
NguyenHuuPhong <phong112068@gmail.com>
2025-09-17 09:30:34 +0700

    của file web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,29 @@
 <?php
+require_once __DIR__ . '/generalRoute.php';
+require_once __DIR__ . '/materDataRoute.php';
+require_once __DIR__ . '/UserRoute.php';
+require_once __DIR__ . '/AuditTrialRoute.php';
+require_once __DIR__ . '/categoryRoute.php';
+require_once __DIR__ . '/ImportRoute.php';
+require_once __DIR__ . '/SchedualRoute.php';
+require_once __DIR__ . '/HistoryRoute.php';
+require_once __DIR__ . '/statisticsRoute.php';
+require_once __DIR__ . '/planRoute.php';
+require_once __DIR__ . '/quotaRoute.php';
 
-    require_once (__DIR__.'/generalRoute.phpgeneralRoute.php');
-    require_once (__DIR__.'/materDataRoute.php');
-    require_once (__DIR__.'/UserRoute.php');
-    require_once (__DIR__.'/AuditTrialRoute.php');
-    require_once (__DIR__.'/categoryRoute.php');
-    require_once (__DIR__.'/ImportRoute.php');
-    require_once (__DIR__.'/SchedualRoute.php');
-    require_once (__DIR__.'/HistoryRoute.php');
-    require_once (__DIR__.'/statisticsRoute.php');
-    require_once (__DIR__.'/planRoute.php');
-    require_once (__DIR__.'/quotaRoute.php');
 ?>
+<!-- <?php
 
+   // require_once (__DIR__.'/generalRoute.php');
+   // require_once (__DIR__.'/materDataRoute.php');
+   // require_once (__DIR__.'/UserRoute.php');
+    //require_once (__DIR__.'/AuditTrialRoute.php');
+   // require_once (__DIR__.'/categoryRoute.php');
+   // require_once (__DIR__.'/ImportRoute.php');
+   // require_once (__DIR__.'/SchedualRoute.php');
+   // require_once (__DIR__.'/HistoryRoute.php');
+   // require_once (__DIR__.'/statisticsRoute.php');
+   // require_once (__DIR__.'/planRoute.php');
+//require_once (__DIR__.'/quotaRoute.php');
+?>
+ -->
