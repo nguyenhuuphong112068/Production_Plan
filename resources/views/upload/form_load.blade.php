d62f570883e58e08d9d865fb29b6b5b6e471e502
NguyenHuuPhong <phong112068@gmail.com>
2025-10-05 17:19:37 +0700

    05/10/2025

--- resources/views/upload/form_load.blade.php
+++ resources/views/upload/form_load.blade.php
@@ -18,3 +18,21 @@
       <button type="submit" name="import">import_permission</button>
       
   </form>
+
+    @if (session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
