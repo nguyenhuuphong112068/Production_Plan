a2ed3a4b538d95caff8682f40afc4422e1668930
NguyenHuuPhong <phong112068@gmail.com>
2025-10-22 15:03:24 +0700

    22/10/2025

--- app/Http/Controllers/Pages/Category/ProductCategoryController.php
+++ app/Http/Controllers/Pages/Category/ProductCategoryController.php
@@ -13,7 +13,7 @@ class ProductCategoryController extends Controller
         public function index(){
 
                 $markets = DB::table('market')->where('active', true)->orderBy('name','asc')->get();
-                $specifications = DB::table('specification')->get();
+                $specifications = DB::table('specification')->orderBy('name','asc')->get();
                 $productNames = DB::table('product_name')->where('active', true)->orderBy('name','asc')->get();
 
                 $intermediate_category = DB::table('intermediate_category')->select('intermediate_category.*','dosage.name as dosage_name' , 'product_name.name as product_name')
