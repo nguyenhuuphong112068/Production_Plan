fff9c0b881b750dcab69479f6f29c298c0c5a836
NguyenHuuPhong <phong112068@gmail.com>
2025-10-23 17:29:19 +0700

    23/10/2025

--- app/Http/Controllers/Pages/Schedual/SchedualController.php
+++ app/Http/Controllers/Pages/Schedual/SchedualController.php
@@ -1544,7 +1544,7 @@ class SchedualController extends Controller
         }// đã có temp
 
         /** Scheduler cho tất cả stage Request */
-        public function scheduleAll( $request) {
+        public function scheduleAll(Request $request) {
 
                 $this->selectedDates = $request->selectedDates??[];
                 $this->work_sunday = $request->work_sunday??false;
@@ -2295,7 +2295,7 @@ class SchedualController extends Controller
         } // đã có temp
 
         public function test(){
-              $this->scheduleAll (null);
+              //$this->scheduleAll (null);
               //$this->createAutoCampain();
               //$this->view (null);
         }
