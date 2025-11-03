9f28ac7de158eef55465db634e21c52827eccb3a
NguyenHuuPhong <phong112068@gmail.com>
2025-11-03 16:58:12 +0700

    03/11/2025

---
+++ resources/views/pages/Schedual/audit/history.blade.php
@@ -0,0 +1,59 @@
+<style>
+  .selectProductModal-modal-size {
+    max-width: 95% !important;
+    width: 95% !important;
+  }
+</style>
+
+<div class="modal fade" id="historyModal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog selectProductModal-modal-size" role="document">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <a href="{{ route('pages.general.home') }}">
+          <img src="{{ asset('img/iconstella.svg') }}" style="opacity: 0.8; max-width:45px;">
+        </a>
+        
+        <h4 class="modal-title w-100 text-center" id="createModal" style="color: #CDC717; font-size: 30px">
+            Lịch Sử Thay Đổi Lịch Sản Xuất
+        </h4>
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Đóng">
+          <span aria-hidden="true">&times;</span>
+        </button>
+
+      </div>
+
+      <div class="modal-body" style="max-height: 100%; overflow-x: auto; ">
+          <div class="card-body">
+            <div class="table-responsive">
+                <table id="data_table_history" class="table table-bordered table-striped" style="font-size: 20px">
+                  <thead >
+                    <tr>
+                        <th>STT</th>
+                        <th>Mã Sản Phẩm</th>
+                        <th>Sản Phẩm</th>
+                        <th>Cở lô</th>
+                        <th>Số Lô</th>
+                        <th>Phòng Sản Xuất</th>
+                        <th>Thới Gian Sản Xuất</th>
+                        <th>Thời Gian Vệ Sinh</th>
+                        <th>Lý Do Tạo Lịch</th>
+                        <th>Người Tạo/ Ngày Tạo</th>
+                        <th>Versions</th>
+                  </tr>
+                  </thead>
+                  <tbody id = "data_table_history_body">
+   
+                  </tbody>
+                </table>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
