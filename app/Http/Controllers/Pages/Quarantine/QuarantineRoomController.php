88e92e1626baaf57000f00d3cf107ce3706709a1
NguyenHuuPhong <phong112068@gmail.com>
2025-11-15 13:57:07 +0700

    15/11/2025

--- app/Http/Controllers/Pages/Quarantine/QuarantineRoomController.php
+++ app/Http/Controllers/Pages/Quarantine/QuarantineRoomController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class QuarantineRoomController extends Controller
 {
@@ -122,6 +123,7 @@ class QuarantineRoomController extends Controller
             ->leftJoin('room','t2.resourceId','room.id')
             ->whereNotNull('t.start')
             ->whereNotNull('t.yields')
+            ->whereNotNull('t.quarantine_room_code')
             ->where('t2.start','>',now())
             ->where('t.active', 1)
             ->where('t.finished', 1)
@@ -136,12 +138,15 @@ class QuarantineRoomController extends Controller
                 'quarantine_room.name',
                 't.yields',
                 't.stage_code',
+    
                 't2.stage_code as next_stage',
                 't2.start as next_start',
                 't2.resourceId as next_room_id',
                 DB::raw("CONCAT(room.code, ' - ', room.name, ' - ', room.main_equiment_name) as next_room"),
                'room.production_group as production_group',
                'room.stage as stage',
+               'room.group_code',
+              
 
             )
             ->orderBy('t.plan_master_id')
@@ -154,28 +159,88 @@ class QuarantineRoomController extends Controller
                 'room_name' => $items->first()->name,
                 'total_yields' => $items->sum('yields'),
                 'details' => $items
-            ];
-        });
-
-        // 3) 🔥 SUM BY NEXT ROOM — tách hẳn ra
-        $sum_by_next_room = $datasRaw->groupBy('next_room')->map(function ($items) {
-            return [
-                'production_group' => $items->first()->production_group,
-                'stage' => $items->first()->stage,
-                'sum_yields' => $items->sum('yields')
+                
             ];
         });
 
 
-        
-        dd ($sum_by_next_room);
+       $sum_by_next_room = DB::table('stage_plan as t')
+            ->leftJoin('stage_plan as t2', function ($join) {
+                $join->on('t2.code','=','t.nextcessor_code');
+            })
+            ->leftJoin('room','t2.resourceId','room.id')
+            ->whereNotNull('t.start')
+            ->whereNotNull('t.yields')
+            ->where('t2.start','>',now())
+            ->where('t.active', 1)
+            ->where('t.finished', 1)
+            ->select(
+                DB::raw("SUM(t.yields) as sum_yields"),
+                DB::raw("CONCAT(room.code, ' - ', room.name, ' - ', room.main_equiment_name) as next_room"),
+                DB::raw("MIN(room.production_group) as production_group"),
+                DB::raw("MIN(room.stage) as stage"),
+                DB::raw("MIN(room.stage_code) as stage_code"),
+                DB::raw("MIN(room.group_code) as group_code"),
+                DB::raw("MIN(room.id) as room_id")
+
+            )
+            ->groupBy('next_room')
+            ->orderBy('group_code')   // sắp xếp theo stage
+            ->get();
+
+        //dd ($sum_by_next_room);
+
         
 
         session()->put(['title' => 'QUẢN LÝ BIỆT TRỮ']);
 
         return view('pages.quarantine.actual.list', [
             'datas' => $datas,
+            'sum_by_next_room' => $sum_by_next_room ,
         ]);
 
+    }
+
+    public function detail(Request $request) {
+
+        Log::info($request->all());
+        $detial = DB::table('stage_plan as t')
+            ->leftJoin('stage_plan as t2', function ($join) {
+                $join->on('t2.code','=','t.nextcessor_code');
+            })
+            ->leftJoin('plan_master','t.plan_master_id','plan_master.id')
+            ->leftJoin('finished_product_category as fc', 't.product_caterogy_id', '=', 'fc.id')
+            ->leftJoin('product_name','fc.product_name_id','product_name.id')
+            ->leftJoin('quarantine_room','t.quarantine_room_code','quarantine_room.code')
+            ->leftJoin('room','t.resourceId','room.id')
+            ->whereNotNull('t.start')
+            ->whereNotNull('t.yields')
+            ->where('t2.resourceId',$request->room_id)
+            ->where('t2.start','>',now())
+            ->where('t.active', 1)
+            ->where('t.finished', 1)
+            ->select(
+                'fc.finished_product_code',
+                'fc.intermediate_code',
+                'product_name.name as product_name',
+                'plan_master.batch',
+                't.quarantine_room_code',
+                'quarantine_room.name',
+                't.yields',
+                't.stage_code',
+                't2.stage_code as next_stage',
+                't2.start as next_start',
+                DB::raw("CONCAT(room.code, ' - ', room.name, ' - ', room.main_equiment_name) as pre_room"),
+                'room.production_group as production_group',
+                'room.stage as stage',
+                'room.group_code',
+              
+            )
+            ->orderBy('t.plan_master_id')
+            ->orderBy('t.stage_code')
+            ->get();
+
+            return response()->json($detial);
+        
     }
 }
--- resources/views/pages/quarantine/actual/dataTable.blade.php
+++ resources/views/pages/quarantine/actual/dataTable.blade.php
@@ -17,41 +17,54 @@
 
 
                 <!-- Sản Lượng thực tế phòng sx tiêp theo -->
-                {{-- <div class="card-body">
-                    <table id="data_table_instrument" class="table table-bordered table-striped">
-                        <thead style="position: sticky; top: 60px; background-color: white; z-index: 1020;">
-                            <tr>
-                                <th>STT</th>
-                                <th>Phòng Sản Xuất</th>
-                                <th>Tên Phòng</th>
-                                <th>Công Đoạn</th>
-                                <th>Tổ</th>
-                                <th>sản Lương Tồn Thực Tế Công Đoạn trước</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                                 @foreach ($details['details'] as $data)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>
-                                            {{ $data->stage_code <= 4 ? $data->intermediate_code : $data->finished_product_code }}
-                                        </td>
-                                        <td>{{ $data->product_name }}</td>
-                                        <td>{{ $data->batch }}</td>
-                                        <td>
-                                        {{ number_format($data->yields, 2) }}
-                                        {{ $data->stage_code <= 4 ? '(Kg)' : '(ĐVL)' }}
-                                        </td>
-                                        <td>{{ $stage_name[$data->next_stage] ?? '' }}</td>
-                                        <td>
-                                            {{ \Carbon\Carbon::parse($data->next_start)->format('d/m/Y H:i') }}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                        </tbody>
-                    </table>
-                </div> --}}
+                <div class="card card-primary mb-4">
+                        <div class="card-header border-transparent">
+                            <h3 class="card-title">
+                               Tồn Kho Phân Bổ Theo Phòng Sản Xuất Ở Công Đoạn Tiếp Theo
+                            </h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
 
+                    <div class="card-body">
+                        <table id="data_table_instrument" class="table table-bordered table-striped">
+                            <thead style="position: sticky; top: 60px; background-color: white; z-index: 1020;">
+                                <tr>
+                                    <th>STT</th>
+                                    <th>Tên Phòng - Thiết Bị Chính</th>
+                                    <th>Công Đoạn Tiếp Theo</th>
+                                    <th>Tổ Quản Lý</th>
+                                    <th>Tồn Thực Tế Công Đoạn trước</th>
+                                    <th class ="text-center">Chi Tiết</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    @foreach ($sum_by_next_room as $key_room => $data)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $data->next_room }}</td>
+                                            <td>{{ $data->stage }}</td>
+                                            <td>{{ $data->production_group }}</td>
+                                            <td>{{ $data->sum_yields }} {{$data->stage_code<=5?"Kg":"ĐVL"}}</td>
+
+                                           <td class="text-center align-middle">
+                                                <button type="button" class="btn btn-primary btn-detial"
+                                                    data-room_id ="{{ $data->room_id }}" data-toggle="modal" data-target="#detailModal">
+                                                    <i class="fas fa-eye"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
                 <!-- Sản Lượng thực tế đang lưu ở từng phòng biệt trữ -->
                 @foreach (collect($datas)->sortKeys() as $quarantine_room_code => $details)
                     <div class="card card-success mb-4">
@@ -81,7 +94,7 @@
                                         <th>Tên Thiết Bị</th>
                                         <th>Số Lô</th>
                                         <th>Sản Lượng Thực Tế</th>
-                                        <th>Công Đoạn Kế Tiếp</th>
+                                        <th>Công Đoạn Tiếp Theo</th>
                                         <th>Thời Gian SX Dự Kiến</th>
                                     </tr>
                                 </thead>
@@ -123,6 +136,61 @@
   $(document).ready(function () {
       document.body.style.overflowY = "auto";
 
+
+    $('.btn-detial').on('click', function() {
+
+        const room_id = $(this).data('room_id');
+       
+        const history_modal = $('#data_table_detail_body')
+
+                // Xóa dữ liệu cũ
+                history_modal.empty();
+
+                // Gọi Ajax lấy dữ liệu history
+                $.ajax({
+                    url: "{{ route('pages.quarantine.actual.detail') }}",
+                    type: 'post',
+                    data: {
+                        room_id: room_id,
+                        _token: "{{ csrf_token() }}"
+                    },
+                    success: function(res) {
+                        if (res.length === 0) {
+                            history_modal.append(
+                                `<tr><td colspan="8" class="text-center">Không có dữ liệu</td></tr>`
+                            );
+                        } else {
+                            res.forEach((item, index) => {
+                                // map màu level
+                        
+                            history_modal.append(`
+                              <tr>
+                                  <td>${index + 1}</td>
+
+                                  <td> 
+                                      <div>${item.intermediate_code ?? ''}</div>
+                                      <div>${item.finished_product_code ?? ''}</div>
+                                  </td>
+
+                                  <td>${item.product_name ?? ''} </td>
+                                  <td>${item.batch ?? ''}</td>
+                                  <td>${(item.pre_room ?? '') + (item.stage_code <= 4 ? "Kg" : "ĐVL")}</td>
+                                  <td>${item.yields ?? ''}</td>
+                                  <td>${item.next_stage ?? ''}</td>
+                                  <td>${moment(item.next_start).format('hh:mm DD/MM/YYYY') ?? ''}</td>
+                                  <td>${item.quarantine_room_code ?? ''}</td>
+                              </tr>
+                          `);
+                            });
+                        }
+                    },
+                    error: function() {
+                        history_modal.append(
+                            `<tr><td colspan="8" class="text-center text-danger">Lỗi tải dữ liệu</td></tr>`
+                        );
+                    }
+                });
+    });
   });
 </script>
 
