9b3ab98a476129388793d8e711447bbdfe8c20a3
NguyenHuuPhong <phong112068@gmail.com>
2026-01-08 15:51:15 +0700

    08/01/2026

--- resources/views/pages/Schedual/audit/dataTable.blade.php
+++ resources/views/pages/Schedual/audit/dataTable.blade.php
@@ -82,7 +82,7 @@
                                 <div> {{ $data->intermediate_code }} </div>
                                 <div> {{ $data->finished_product_code }} </div>
                             </td>
-                            <td>{{ $data->product_name . ' ' . $data->batch }}</td>
+                            <td>{{ $data->product_name . '-' . $data->batch }}</td>
                             <td>{{ $data->batch_qty . ' ' . $data->unit_batch_qty }}</td>
                             <td>{{ $data->batch }} </td>
 
