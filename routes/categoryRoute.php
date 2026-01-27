31448583489b10812a2bcce9a7f5efd396b0fbf6
NguyenHuuPhong <phong112068@gmail.com>
2026-01-27 12:27:29 +0700

    27/01/2026

--- routes/categoryRoute.php
+++ routes/categoryRoute.php
@@ -35,6 +35,7 @@ Route::prefix('/category')
             Route::post('update', 'update')->name('update');
             Route::post('deActive','deActive')->name('deActive'); 
 
+                
             Route::get('getJsonFPCategogy','getJsonFPCategogy')->name('getJsonFPCategogy'); 
     });
 
