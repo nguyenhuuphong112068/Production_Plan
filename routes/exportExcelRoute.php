5e4ca59b88403f3b1677b828019092f92d8b3c2b
NguyenHuuPhong <phong112068@gmail.com>
2026-01-28 15:15:35 +0700

    28/01/2026

--- routes/exportExcelRoute.php
+++ routes/exportExcelRoute.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Controllers\Pages\Category\ProductCategoryController;
-
+use App\Http\Controllers\Pages\Plan\ProductionPlanController;
 use Illuminate\Support\Facades\Route;
 
-
+///exportExcel/Plan_feekback/open_feedback_API
 Route::prefix('/exportExcel')
 ->name('pages.exportExcel.')
 ->group(function(){
@@ -14,6 +14,13 @@ Route::prefix('/exportExcel')
     ->controller(ProductCategoryController::class)
     ->group(function(){
             Route::get('getJsonFPCategory','getJsonFPCategory')->name('getJsonFPCategory'); 
+    });
+
+     Route::prefix('/Plan_feekback')
+    ->name('Plan_feekback.')
+    ->controller(ProductionPlanController::class)
+    ->group(function(){
+            Route::get('open_feedback_API','open_feedback_API')->name('open_feedback_API'); 
     });
 
 
