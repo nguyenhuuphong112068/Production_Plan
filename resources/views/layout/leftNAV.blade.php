a8fa28d74cd49cd7b6185402655037f37180e717
Nguyen Huu Phong <phong112068@gmail.com>
2026-09-14 13:54:29 +0700

    14/09/2026

--- resources/views/layout/leftNAV.blade.php
+++ resources/views/layout/leftNAV.blade.php
@@ -634,14 +634,15 @@
                                 <p>Chính Sách Sản Lượng</p>
                             </a>
                         </li>
-
-                        {{-- <li class="nav-item">
-                            <a href="{{ route('pages.Schedual.audit.index') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Lịch Sử Thay Đổi Lịch</p>
-                            </a>
-                        </li>
-                        @if (user_has_permission(session('user')['userId'], 'layout_test', 'boolean'))
+                        @if (user_has_permission(session('user')['userId'], 'schedual_audit', 'boolean'))
+                            <li class="nav-item">
+                                <a href="{{ route('pages.Schedual.audit.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Lịch Sử Thay Đổi Lịch</p>
+                                </a>
+                            </li>
+                        @endif
+                        {{-- @if (user_has_permission(session('user')['userId'], 'layout_test', 'boolean'))
                             <li class="nav-item">
                                 <a href="{{ route('pages.Schedual.audit.compare') }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
